3bfda70d4720de8e0858a13f83a2a44208d548ce
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-08-09 21:13:04 +0200

    moved file handling out of analyse() function

--- commit_stats.py
+++ commit_stats.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from datetime import datetime
 from csv import DictWriter
-from typing import Optional
-
+from typing import Optional, Iterable
 from tqdm import tqdm
 
 from commits import list_commits, timestamp, checkout
@@ -22,30 +21,32 @@ class CommitStats:
     mean_cyclomatic_complexity: float
 
 
-def analyze_history(repo: Path, max_history: int, exclude: Optional[str], csv_output_file: Path) -> None:
-    with csv_output_file.open(mode="w", newline="") as f:
-        csv_writer = DictWriter(f, fieldnames=[field.name for field in fields(CommitStats)])
-        csv_writer.writeheader()
-        for commit in tqdm(list_commits(repo=repo)[:max_history], desc="Writing Commits to CSV"):
-            checkout(repo, commit)
-            python = [stat for stat in count_lines(repo, exclude_pattern=exclude) if stat.language == 'Python'][0]
-            stats = CommitStats(
-                hash=commit,
-                timestamp=timestamp(repo, commit),
-                language=python.language,
-                files=python.files,
-                loc=python.lines_of_code,
-                comments=python.comments,
-                mean_cyclomatic_complexity=mean_cyclomatic_complexity(repo, exclude_pattern=exclude)
-            )
-            csv_writer.writerow(asdict(stats))
+def analyze_history(repo: Path, max_history: int, exclude: Optional[str]) -> Iterable[CommitStats]:
+    """Yields a Python CommitStats object for each commit in the repo's history."""
+    for commit in tqdm(list_commits(repo=repo)[:max_history], desc="Writing Commits to CSV"):
+        checkout(repo, commit)
+        python = [stat for stat in count_lines(repo, exclude_pattern=exclude) if stat.language == 'Python'][0]
+        yield CommitStats(
+            hash=commit,
+            timestamp=timestamp(repo, commit),
+            language=python.language,
+            files=python.files,
+            loc=python.lines_of_code,
+            comments=python.comments,
+            mean_cyclomatic_complexity=mean_cyclomatic_complexity(repo, exclude_pattern=exclude)
+        )
 
 
 
 if __name__ == '__main__':
-    repo = Path("D:/ProgrammingProjects/suite2p2/suite2p")
-    max_history = 3
-    exclude = "*gui*.py"
-    csv_output_file = Path("./stats.csv")
 
-    analyze_history(repo=repo, max_history=max_history, exclude=exclude, csv_output_file=csv_output_file)
+    repo = Path("D:/ProgrammingProjects/suite2p2/suite2p")
+    max_history = 30
+    exclude = "*gui*.py"
+    csv_output_file = Path("./stats2.csv")
+
+    with csv_output_file.open(mode="w", newline="") as f:
+        csv_writer = DictWriter(f, fieldnames=[field.name for field in fields(CommitStats)])
+        csv_writer.writeheader()
+        for stats in analyze_history(repo=repo, max_history=max_history, exclude=exclude):
+            csv_writer.writerow(asdict(stats))
--- cyclomatic_complexity.py
+++ cyclomatic_complexity.py
@@ -5,10 +5,9 @@ from subprocess import Popen, PIPE
 def mean_cyclomatic_complexity(path: Path, exclude_pattern: Optional[str] = None) -> float:
     cmd = f'radon cc {path} -a --exclude={exclude_pattern}' if exclude_pattern else f'radon cc {path} -a'
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    last_line = proc.stdout.readlines()[-2].strip().decode()
+    lines = proc.stdout.readlines()
+    last_line = lines[-2].strip().decode()
     cc = float(last_line.split('(')[-1].split(')')[0])
     return cc
 
 
-repo = Path("D:/ProgrammingProjects/suite2p2")
-print(mean_cyclomatic_complexity(repo))
