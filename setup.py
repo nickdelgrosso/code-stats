0530ad2fdeebbd905deb3e811a2e272dad0db1df
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-08-09 23:56:39 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='code-stats',
+    version='0.0.1',
+    packages=['code-stats'],
+    url='https://www.github.com/nickdelgrosso/code-stats',
+    license='',
+    author='Nicholas A. Del Grosso',
+    author_email='delgrosso.nick@gmail.com',
+    description=''
+)
